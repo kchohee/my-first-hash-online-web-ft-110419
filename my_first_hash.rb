fdea277a80161b2704592a505e995b00900ea8d9
Marie (Cho) Hughes <kchohee0630@gmail.com>
2019-11-09 03:34:37 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -9,13 +9,8 @@ def shipping_manifest
 end
 
 def retrieval
-  shipping_manifest = {
-                  "whale bone corsets" => 5,
-                  "porcelain vases" => 2,
-                  "oil paintings" => 3
-                 }
- puts hash["oil paintings"]
-
+  shipping_manifest = {"whale bone corsets" => 5,"porcelain vases" => 2, "oil paintings" => 3 }
+shipping_manifest["oil paintings"]
 end
 
 def adding
