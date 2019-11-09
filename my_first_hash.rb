a895828791a65a32e00e6a175f05b2f9480d5c72
Marie (Cho) Hughes <kchohee0630@gmail.com>
2019-11-09 03:38:30 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -14,19 +14,8 @@ shipping_manifest["oil paintings"]
 end
 
 def adding
-  shipping_manifest = {
-                  "whale bone corsets" => 5,
-                  "porcelain vases" => 2,
-                  "oil paintings" => 3
-                 }
-  # add 2 muskets to the shipping_manifest hash below
-
-
-
-  # add 4 gun powder to the shipping_manifest hash below
-
-
-
-  # return the shipping_manifest hash below
-
+  shipping_manifest = {"whale bone corsets" => 5,"porcelain vases" => 2,"oil paintings" => 3}
+  shipping_manifest["muskets"] =5
+  shipping_manifest["gun powder"] =4
+  return shipping_manifest
 end
